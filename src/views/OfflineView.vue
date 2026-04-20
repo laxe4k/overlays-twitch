2a101c99c4dbae5ae75224afeb68c9d57e08c657
Laxe4k <laxe4k@hotmail.com>
2026-04-20 09:04:08 +0200

    Adds 'Pause' and 'Offline' routes with new Offline view
    Introduces a new 'Offline' route and its corresponding view to display an offline message using reusable components. Reinstates the 'Pause' route for consistency in navigation.
    
    Enhances user experience by improving route structure and adding a dedicated offline state view.

---
+++ src/views/OfflineView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import DotField from '@/components/DotField.vue'
+import OverlayContent from '@/components/OverlayContent.vue'
+</script>
+
+<template>
+  <DotField />
+  <OverlayContent line1="LE LIVE EST" line2="OFFLINE" subtitle="À BIENTÔT" />
+</template>
